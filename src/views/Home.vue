efe2c48ffc2ba147221415d70abe2063377ec9aa
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.lan1>
2021-11-26 18:23:21 -0600

    add boiler plate, add app element and data field.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Photo finder</h1>
+  <div id="app-instasearch">
+    <div class="input-container">
+      <input ty[e="text" placeholder="Type a name" />]
+    </div>
+    <ul>
+      <li class="photo">
+        <img src="" />
+        <span class="author"> Some Names </span>
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var instasearchApp = new Vue({
+  el: '#app=instasearch',
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: {
+    authorNameSearchString: "",
+    photoFeed: null 
   }
-}
+})
 </script>
+
+<style>
+.photo {
+	list-style: none;
+	display: grid;
+	grid-template-columns: 200px auto;
+	margin-top: 20px;
+	align-items: center;
+	background-color: #e9edf0;
+	padding: 30px 50px;
+	border-radius: 5px;
+	border: 1px solid #e3e3e3;
+}
+
+.author {
+	font-size: 25px;
+	margin-left: 20px;
+	color: #75818e;
+}
+
+.photo img {
+	border-radius: 5px;
+	width: 200px;
+}
+
+.input-container {
+	border-radius: 5px;
+	background: #677482;
+	padding: 10px;
+}
+
+.input-container input {
+	border: none;
+	background: transparent;
+	color: white;
+	padding: 6px 15px;
+	font-size: 18px;
+}
+
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #a6b0ba;
+  opacity: 1; /* Firefox */
+}
+</style>
